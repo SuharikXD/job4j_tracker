4214350e258b64a6854065f13292ea46b1c9152a
Данил <SuharikXD@yandex.ru>
2021-09-07 10:37:18 +0300

    2.2. Рефакторинг - Площадь треугольника. [#122642 #199624]

---
+++ src/test/java/ru/job4j/oop/PointTest.java
@@ -0,0 +1,12 @@
+package ru.job4j.oop;
+
+public class PointTest {
+
+    public void testMain() {
+        Point a = new Point(0, 0);
+        Point b = new Point(0, 2);
+        Point z = new Point(2, 0);
+       // double rsl = b.distance3d();
+        //return rsl;
+    }
+}
