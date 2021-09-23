e1cc229025500df9b0130adc17e705985c134b94
Данил <SuharikXD@yandex.ru>
2021-09-23 11:44:52 +0300

    5. Конвертация листа массивов в один лист Integer [#10037]

--- src/test/java/ru/job4j/search/PhoneDictionaryTest.java
+++ src/test/java/ru/job4j/search/PhoneDictionaryTest.java
@@ -1,9 +1,11 @@
 package ru.job4j.search;
 
+import org.junit.Assert;
 import org.junit.Test;
 import java.util.ArrayList;
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
 
 public class PhoneDictionaryTest {
     @Test
@@ -14,5 +16,15 @@ public class PhoneDictionaryTest {
         );
         ArrayList<Person> persons = phones.find("Petr");
         assertThat(persons.get(0).getSurname(), is("Arsentev"));
+    }
+
+    @Test
+    public void whenNotFind() {
+        PhoneDictionary phones = new PhoneDictionary();
+        phones.add(
+                new Person("Petr", "Arsentev", "534872", "Bryansk")
+        );
+        ArrayList<Person> persons = phones.find("Alexey");
+        Assert.assertTrue(persons.isEmpty());
     }
 }
