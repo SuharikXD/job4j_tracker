e0e1546caf05abfa5a78ce0d225da761ced94f29
Данил <SuharikXD@yandex.ru>
2021-10-13 17:10:21 +0300

    0. Stream API [#58166 #215449]

--- src/main/java/ru/job4j/stream/StreamFilter.java
+++ src/main/java/ru/job4j/stream/StreamFilter.java
@@ -9,7 +9,7 @@ public class StreamFilter {
     public static void main(String[] args) {
         List<Integer> numbers = new ArrayList<>(Arrays.asList(-1, -3, 4, 5, 6, -99, 12));
         List<Integer> result = numbers.stream().filter(
-                integer -> integer >= 0
+                integer -> integer > 0
         ).collect(Collectors.toList());
         result.forEach(System.out::println);
     }
