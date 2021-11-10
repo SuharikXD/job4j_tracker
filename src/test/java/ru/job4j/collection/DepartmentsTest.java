5a218afb4422ff472d1d5c61912138eae9efd45c
Данил <SuharikXD@yandex.ru>
2021-11-10 15:40:09 +0300

    1. Метод .of [#70623]

--- src/test/java/ru/job4j/collection/DepartmentsTest.java
+++ src/test/java/ru/job4j/collection/DepartmentsTest.java
@@ -12,16 +12,16 @@ import static org.junit.Assert.assertThat;
 public class DepartmentsTest {
     @Test
     public void whenMissed() {
-        List<String> input = Arrays.asList("k1/sk1");
-        List<String> expect = Arrays.asList("k1", "k1/sk1");
+        List<String> input = List.of("k1/sk1");
+        List<String> expect = List.of("k1", "k1/sk1");
         List<String> result = Departments.fillGaps(input);
         assertThat(result, is(expect));
     }
 
     @Test
     public void whenNonChange() {
-        List<String> input = Arrays.asList("k1", "k1/sk1");
-        List<String> expect = Arrays.asList("k1", "k1/sk1");
+        List<String> input = List.of("k1", "k1/sk1");
+        List<String> expect = List.of("k1", "k1/sk1");
         List<String> result = Departments.fillGaps(input);
         assertThat(result, is(expect));
     }
@@ -29,7 +29,7 @@ public class DepartmentsTest {
     @Test
     public void depAscTest() {
         List<String> input = Arrays.asList("K2", "K1", "K2/SK1");
-        List<String> expect = Arrays.asList("K1", "K2", "K2/SK1");
+        List<String> expect = List.of("K1", "K2", "K2/SK1");
         Departments.sortAsc(input);
         assertThat(input, is(expect));
     }
@@ -38,7 +38,7 @@ public class DepartmentsTest {
     public void depAscTwoTest() {
         List<String> input = Arrays.asList("K2", "K1/SK1", "K1", "K1/SK1/SSK1",
                 "K2/SK1/SSK1", "K2/SK1/SSK2", "K2/SK1");
-        List<String> expect = Arrays.asList("K1", "K1/SK1", "K1/SK1/SSK1",
+        List<String> expect = List.of("K1", "K1/SK1", "K1/SK1/SSK1",
                 "K2", "K2/SK1", "K2/SK1/SSK1", "K2/SK1/SSK2");
         Departments.sortAsc(input);
         assertThat(input, is(expect));
@@ -47,7 +47,7 @@ public class DepartmentsTest {
     @Test
     public void depDescTest() {
         List<String> input = Arrays.asList("K2", "K1", "K2/SK1");
-        List<String> expect = Arrays.asList("K2", "K2/SK1", "K1");
+        List<String> expect = List.of("K2", "K2/SK1", "K1");
         Departments.sortDesc(input);
         assertThat(input, is(expect));
     }
